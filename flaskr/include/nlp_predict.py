e43c234bb0dd9362f50442033f3ede299772cfa7
kuc-arc-f <naka@kuc-arc-f.com>
2019-03-16 19:23:43 +0900

    first commit

---
+++ flaskr/include/nlp_predict.py
@@ -0,0 +1,67 @@
+# -*- coding: utf-8 -*-
+# predict
+
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.metrics.pairwise import cosine_similarity
+import numpy as np
+
+from janome.tokenizer import Tokenizer
+from pandas import Series, DataFrame
+import pandas as pd
+import pickle
+#
+class NlpPredict:
+    def __init__(self):
+        self.params = {}
+        self.words=[]
+        self.docs=None
+        #self.answers =[]
+        self.get_data()
+        self.vecs=None
+    #
+    def get_token(self, text):
+        t = Tokenizer()
+        tokens = t.tokenize(text, wakati=True)  # 分かち書きする
+        word = ""
+        for token in tokens:
+            word +=token  + " "
+        return word
+    #
+    def get_data(self):
+        csv_data = pd.read_csv("flaskr/files/bun_data.csv" ,encoding="SHIFT-JIS" )
+        self.words=csv_data["bun"]
+        tokens=[]
+        for item in csv_data["bun"]:
+            #print(item)
+            token=self.get_token(item)
+            tokens.append(token)
+        self.docs = np.array(tokens)
+    #
+#    def train(self, docs ):
+#        self.vectorizer = TfidfVectorizer(use_idf=True, token_pattern=u'(?u)\\b\\w+\\b')
+#        self.vecs = self.vectorizer.fit_transform(docs )
+#        return ""
+    #
+    def get_vectorize(self):
+        file_name="flaskr/files/params.pkl"
+        self.vectorizer =None
+        with open(file_name, 'rb') as f:
+            self.vectorizer = pickle.load(f)
+        print("load vectorizer OK!!")
+        self.vecs= self.vectorizer.transform( self.docs )
+        return self.vectorizer
+    #
+    def predict(self, str):
+        instr = self.get_token(str ).strip()
+        #print("instr=", instr )
+        x= self.vectorizer.transform( [  instr ])
+        #print( "x=",x)
+        #Cosine類似度（cosine_similarity）の算出
+        num_sim=cosine_similarity(x , self.vecs)
+        #print(num_sim )
+        index = np.argmax( num_sim )
+        #
+        #print("word=", self.words[index])
+        #print()
+        ret= self.words[index]
+        return ret
